fe68019ddfe927fd1dea83ea551f1e7fa74447c2
marthinusswart <matt.swart@icloud.com>
2024-03-18 21:55:31 +1100

    removed .class

--- src/main/java/com/mattswart/bankstatementprocessor/BankstatementProcessorServiceApplication.java
+++ src/main/java/com/mattswart/bankstatementprocessor/BankstatementProcessorServiceApplication.java
@@ -8,6 +8,7 @@ public class BankstatementProcessorServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankstatementProcessorServiceApplication.class, args);
+		//test
 	}
 
 }
