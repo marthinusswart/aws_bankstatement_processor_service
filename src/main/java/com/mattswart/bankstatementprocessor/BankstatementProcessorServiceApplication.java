70d4b91df418db1598a926526a75e26cd6707b8c
marthinusswart <matt.swart@icloud.com>
2024-03-18 21:50:37 +1100

    initial checkin

---
+++ src/main/java/com/mattswart/bankstatementprocessor/BankstatementProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mattswart.bankstatementprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankstatementProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankstatementProcessorServiceApplication.class, args);
+	}
+
+}
