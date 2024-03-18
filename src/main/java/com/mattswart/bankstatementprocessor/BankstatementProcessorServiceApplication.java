5cafecc2e7fc39bdbeb88613178bbe348ab9d69e
marthinusswart <matt.swart@icloud.com>
2024-03-18 22:27:49 +1100

    moved some files

--- src/main/java/com/mattswart/bankstatementprocessor/BankstatementProcessorServiceApplication.java
+++ src/main/java/com/mattswart/bankstatementprocessor/BankstatementProcessorServiceApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankstatementProcessorServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankstatementProcessorServiceApplication.class, args);
-		//test
+		SpringApplication.run(BankstatementProcessorServiceApplication.class, args);		
 	}
 
 }
